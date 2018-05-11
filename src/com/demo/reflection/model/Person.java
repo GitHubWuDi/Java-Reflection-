9dc7858be2bd2619bdece1bba14a2df8a5c8ef4c
wudi <wudi891012@163.com>
2018-05-11 23:16:10 +0800

    添加person实体

---
+++ src/com/demo/reflection/model/Person.java
@@ -0,0 +1,48 @@
+package com.demo.reflection.model;
+
+/**
+ * @author wudi
+ * @version 创建时间：2018年5月11日 下午11:00:05
+ * @ClassName Person
+ * @Description 类描述
+ */
+public class Person {
+
+	private String guid;
+	private String name;
+	private Integer age;
+	private String sex;
+
+	public String getGuid() {
+		return guid;
+	}
+
+	public void setGuid(String guid) {
+		this.guid = guid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+}
